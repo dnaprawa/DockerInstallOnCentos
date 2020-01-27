7a1e170de42270b254fd19ea909b3730894f9582
Damian N <dnaprawa@gmail.com>
2020-01-27 14:05:56 +0100

    Init

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo usermod -aG docker $(whoami)
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo chown root:docker /var/run/docker.sock
+sudo newgrp docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
